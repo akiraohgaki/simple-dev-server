0b91b29eea04db5340f8db47e83aa43653f9e8d2
Akira Ohgaki <akiraohgaki@gmail.com>
2021-03-04 00:46:46 +0900

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,13 +1,4 @@
-export interface ServerConfigOptions {
-    port?: number;
-    documentRoot?: string;
-    directoryIndex?: string;
-    headers?: {[key: string]: string};
-    mimeTypes?: {[key: string]: string};
-    rewriteRules?: Array<[string, string]>;
-}
-
-export interface ServerConfig extends ServerConfigOptions {
+export interface ServerConfig {
     port: number;
     documentRoot: string;
     directoryIndex: string;
