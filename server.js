ee37242d9af18e0b33e379b193df416480a52b0e
Akira Ohgaki <akiraohgaki@gmail.com>
2021-03-04 00:47:33 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import {SimpleDevServer} from './dist/simple-dev-server.js';
 const server = new SimpleDevServer({
     documentRoot: process.cwd() + '/public',
     rewriteRules: [
-        ['^/[^.]+$', '/index.html']
+        ['/[^.]+$', '/index.html']
     ]
 });
 server.start();
