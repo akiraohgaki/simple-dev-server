e97400f630021698412e177d79645bfda3df1282
Akira Ohgaki <akiraohgaki@gmail.com>
2021-03-17 03:03:18 +0900

    Update dist files

---
+++ dist/lib/config.d.ts
@@ -0,0 +1,2 @@
+import type { ServerConfig } from './types.js';
+export declare const defaultConfig: ServerConfig;
---
+++ dist/lib/config.js
@@ -0,0 +1,44 @@
+export const defaultConfig = {
+    hostname: 'localhost',
+    port: 8080,
+    documentRoot: process.cwd(),
+    directoryIndex: 'index.html',
+    rewriteRules: [],
+    headers: {
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache'
+    },
+    mimeTypes: {
+        'txt': 'text/plain',
+        'htm': 'text/html',
+        'html': 'text/html',
+        'css': 'text/css',
+        'js': 'text/javascript',
+        'eot': 'application/vnd.ms-fontobject',
+        'ttf': 'font/ttf',
+        'otf': 'font/otf',
+        'woff': 'font/woff',
+        'woff2': 'font/woff2',
+        'svg': 'image/svg+xml',
+        'ico': 'image/vnd.microsoft.icon',
+        'gif': 'image/gif',
+        'jpg': 'image/jpeg',
+        'jpeg': 'image/jpeg',
+        'jfif': 'image/jpeg',
+        'pjpeg': 'image/jpeg',
+        'pjp': 'image/jpeg',
+        'png': 'image/png',
+        'apng': 'image/apng',
+        'webp': 'image/webp',
+        'avif': 'image/avif',
+        'wav': 'audio/wav',
+        'ogg': 'application/ogg',
+        'oga': 'audio/ogg',
+        'ogv': 'video/ogg',
+        'weba': 'audio/webm',
+        'webm': 'video/webm',
+        'json': 'application/json',
+        'xml': 'application/xml'
+    }
+};
+//# sourceMappingURL=config.js.map
